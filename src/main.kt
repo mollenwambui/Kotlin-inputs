775c63fbd16f056447fd115a23a38ed59c83e140
mollenwambui <mollenwambui@gmail.com>
2022-03-30 17:01:07 +0300

    first commit

---
+++ src/main.kt
@@ -0,0 +1,21 @@
+fun main(){
+    wow()
+    love()
+    classy()
+}
+ fun wow(){
+     print("Enter text:  ")
+     val life= readLine()
+     println("you have entered $life")
+ }
+fun love(){
+    print("Enter a text: ")
+    val food= readLine()
+    println("This is the statement Mollen  said-$food")
+}
+fun classy(){
+    print("Enter text:  ")
+    val lace= readLine()
+    println("I really hope we will improve this class called $lace")
+
+}
